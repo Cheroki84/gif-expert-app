677cdde4bdf6d50d42e38fd25e12b8c58942e77d
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2021-04-12 17:39:10 +0200

    Created AddCategory component. Added PropTypes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
+
 const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Dragon Ball', 'One Piece', 'Pokemon']);
+    
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'Naruto']);
+    //     setCategories(cats => [...cats, 'Naruto']);
+    // }
+
     return (
         <>
         <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories} />
         <hr/>
+
+
+        <ol>
+            {
+                categories.map(category => {
+                    return <li key={category} >{category}</li>
+                })
+            }
+        </ol>
+
         </>
     )
 }
