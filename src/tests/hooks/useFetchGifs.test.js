c8b2b3dc645faff0668c1c5dc6fc5b66bd87ff86
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2021-04-16 18:01:00 +0200

    added another test for the customHook useFetchGifs

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,14 +3,28 @@ import { useFetchGifs } from '../../hooks/useFetchGifs';
 
 describe('Pruebas en el custom hook useFetchGifs', () => {
     
-    test('Debe de devolver el estado inicial', () => {
+    test('Debe de devolver el estado inicial', async() => {
         
-        const {result} = renderHook(() => useFetchGifs('Dragon Ball'));
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('Dragon Ball'));
 
         const {data, loading} = result.current;
 
+        await waitForNextUpdate();
+
         expect(data).toEqual([]);
         expect(loading).toBe(true);
+
+    });
+    
+    test('Debe de devolver un array de imgs y el loading en false', async() => {
+        
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('Dragon Ball'));
+        await waitForNextUpdate();
+
+        const {data, loading} = result.current;
+
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
 
     });
     
